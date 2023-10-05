ff74eb2d969b2e0244ca9ca4b7f395c37cb6ed04
Eric Grossman <ericg5455@gmail.com>
2023-10-04 21:38:41 -0700

    feat(nav): make styling/content dynamic

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+	{
+		name: 'Home',
+		path: '/',
+	},
+	{
+		name: 'Journal',
+		path: '/journal/',
+	},
+	{
+		name: 'About',
+		path: '/about/',
+	},
+];
+
+export default navData;
